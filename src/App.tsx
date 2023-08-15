df36351aa3663666a6bf50af07706466ae1a1624
kira <kira@fanruan.com>
2023-08-15 18:08:10 +0800

    feat: 实现 #none

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import classNames from "classnames";
+import { useState } from "react";
+
 export function App() {
+  const [selected, setSelected] = useState(false);
+
   return (
-    <div className="h-full w-full flex items-center justify-center ">点赞</div>
+    <div className="h-full w-full flex items-center justify-center">
+      <span
+        className={classNames("like-button", {
+          selected,
+        })}
+        onClick={() => setSelected(!selected)}
+      ></span>
+    </div>
   );
 }
